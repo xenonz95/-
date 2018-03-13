fe60c3da6f55a0e38cd0c16d264b86cd0b735335
xenonz95 <xenon95@live.com>
2018-03-13 09:21:34 +0000

    [auto]test

---
+++ work.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [ "$1" = "commit" ];
+then
+	git add . -A --ignore-errors
+	git commit -m [auto]$2
+	exit 0
+fi
+
+if [ "$1" = "push" ];
+then
+	git push origin master
+	exit 0
+fi
+
+
+
+root=$(pwd)
+alldir=$(ls | grep -E  "+[0-9]+")
+
+
+for dir in $alldir
+	do
+		cd ./$dir
+		echo 已经进入 $dir
+		make  $1
+		cd $root
+	
+	done
