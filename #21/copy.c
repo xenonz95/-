fab497d12ba1f50e16a5f16b6490706df23476f4
xenonz95 <xenon95@live.com>
2018-04-19 15:51:14 +0800

    [script]

--- #21/copy.c
+++ #21/copy.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 //malloc
 #include <stdlib.h>
 //for open
@@ -7,28 +6,88 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+
 #include <unistd.h>
 #include <sys/time.h>
 
 #include <string.h>
+
+#define block 512*8
+unsigned char buf[block];
 
 typedef unsigned char bool;
 char* color[] = {"\033[0m", "\033[31m\033[1m", "\034[31m\033[1m", "\033[41m", "\033[44m"};//0 = normal 1 = red_font 2 = blue_font 3 = red_background 4 = blue_background
 char* bar = "==================================================";
 char* space = "                                                  ";
 unsigned int my_error = 0;//1 = open_fail 2 = get_stat_fail 3 = parameter_error
+unsigned int my_warning = 0;// 1 = stack_empty 2 = queue_empty 
 
-void print_progress(double d1, double d2)
+char* path_stack[65535] = {NULL,};
+int stack_top = 0;
+char* path_queue[65535] = {NULL,};
+int queue_head = 0;
+int queue_end = 0;
+
+
+bool push(char* str)
 {
+	if(stack_top >= 65535)
+		return -1;
+	if(path_stack[stack_top] != NULL)
+	{
+		free(path_stack[stack_top]);
+		path_stack[stack_top] = NULL;
+	}
+	path_stack[stack_top++] = strdup(str);
+	return 0;
+}
+char* pop()
+{
+	if(stack_top < 0)
+	{
+		my_warning = 1;
+		return NULL;
+	}
+	return path_stack[--stack_top];
+}
+
+bool enter(char* str)
+{
+	if(queue_head >= 65535)
+	{
+		return -1;
+	}
+	if(path_queue[queue_head] != NULL)
+	{
+		free(path_queue[queue_head]);
+		path_queue[queue_head] = NULL;
+	}
+	path_queue[queue_head++] = str;
+	return 0;
+}
+
+char* leave()
+{
+	if(queue_end == queue_head)
+	{
+		my_warning = 2;
+		return NULL;
+	}
+	return path_queue[++queue_end];
+}
+
+int print_progress(double d1, double d2)
+{
+	//d1 is front, and d2 is background
 	static int old_p1 = 0, old_p2 = 0;
 	int progress1 = d1*50;
 	int progress2 = d2*50;
-	
 	if(old_p1 == progress1 && old_p2 == progress2)
-		return;
+	{
+		return 1;
+	}
 	old_p1 = progress1;
 	old_p2 = progress2;
-
 	if(progress1 > progress2)
 	{
 		printf("\r[%s%s%s",color[4], bar+50-progress2, color[0]);
@@ -41,13 +100,10 @@ void print_progress(double d1, double d2)
 		printf("%s%s%s",color[4], space+50-(progress2-progress1), color[0]);
 		printf("%s]",space+50-(50-progress2));
 		printf("%3d%%",(int)(d2*100));
-
 	}
-	//printf("\t%s", cpname);
 	fflush(stdout);
-
+	return 0;
 }
-
 
 bool is_dir(char* file_name)
 {
@@ -63,7 +119,7 @@ bool is_dir(char* file_name)
 	}
 	if(S_IFDIR & statbuf.st_mode)
 		return 1;
-	else 
+	else
 		return 0;
 }
 
@@ -82,14 +138,45 @@ long get_size(char* file_name)
 	return statbuf.st_size;
 }
 
+bool copy_bath(char* dest, char* src,int size)
+{
+	double dsize = size;
+	int fdr = 0;
+	int fdw = 0;
+	int ret = 0;
+	int cpd = 0;
+	fdr = open(src, O_RDONLY);
+	fdw = open(dest, O_RDWR | O_TRUNC | O_CREAT, S_IRUSR|S_IWUSR | S_IRGRP | S_IROTH );
+	while(1)
+	{
+		ret = read(fdr, buf, block);
+		cpd += ret;
+		write(fdw, buf, ret);
+		if(print_progress(cpd/dsize, cpd/dsize) != 1)
+			printf("  %-25.25s|", src);
+		if(cpd == size)
+			break;
+	}
+	close(fdr);
+	close(fdw);
+}
+
+void get_dir_size(char* dest, char* src)
+{
+	long dir_size = 0;
+
+	//首先深度遍历源文件夹，若目的目录不存在则创建目录，读取文件大小后将文件相对地址插入队列
+	//深度优先遍历时使用栈存储工作目录相对地址
+}
+
 int main(int argc, char* const argv[])
 {
 	//prepare something
-	int block = 512*8;
 	int ret = 0;
 	char cpname[256] = "MVI_5612.mp4";
 	char ptname[256] = "paste.mp4";
 	long file_size = 0;
+	long dir_size = 0;
 	int test_mode = 0;
 	bool dir_mode = 0;
 
@@ -111,10 +198,8 @@ int main(int argc, char* const argv[])
 				my_error = 3;
 				printf("[error]a wrong parameter\n");
 				break;
-
 			default:break;
 		}
-
 	}
 	if(is_dir(cpname))
 	{
@@ -132,19 +217,18 @@ int main(int argc, char* const argv[])
 	if(dir_mode == 0)
 	{
 		file_size = get_size(cpname);
-		
+		copy_bath(ptname, cpname, file_size);
 	}
-	int i = 0;
-	int  j = 0;
-	for(i = 0; i<100; i++)
-	{
-		for(j = 0;j<100; j++)
-		{
-			print_progress(j/100.0,i/100.0);
-			usleep(10000);
-		}
-	}
-	putchar('\n');
+	else{
 
+	}
+
+
+
+
+
+
+	
+	putchar('\n');
 	return 0;
 }
